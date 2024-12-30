36edfa86c5f6ef69207156f6a7c6bc3f52cd354c
Afsna Akter <tarinakter567@gmail.com>
2024-12-30 22:18:04 +0600

    update student management system

--- lib/module_5_assignment.dart
+++ lib/module_5_assignment.dart
@@ -1,29 +1,82 @@
+abstract class Role{
+  void displayRole();
+}
 
+class Person{
+  String name;
+  int age;
+  String address;
 
-class StudentInfo{
-  String name = 'Jhon Doe';
-  String address ='123 Main Street';
-  List<int> scors =[90,85,82];
-  double average = 0;
+  Person(this.name, this.age, this.address);
 
-  StudentInfo(){
-   // average = scors.reduce((value, element) => value + element) / scors.length;
-    int sumScors = 0;
-    for(int score in scors){
-      sumScors += score;
+  String getName()=>name;
+  int getAge()=>age;
+  String getAddress()=>address;
+
+  void displayRole(){
     }
-    //print('${sumScors / scors.length}');
-    average = sumScors / scors.length;
+}
 
+class Student extends Person{
+String StudentId;
+String grade;
+List<double> course;
+
+Student(this.StudentId, this.grade, this.course):super('John Doe', 20, ' 123 Main St');
+
+@override
+  void displayRole() {
+    print('Role: Student');
   }
-   
+  
+  double clculateAvg(){
 
+    double avg =0.0;
+    try{
+      for(int i =0 ; i<=course.length; i++){
+      avg += course[i];
+    }
+    }catch(e){}
+    return avg/course.length;
+  }
+}
+
+class Teacher extends Person{
+  String TeacherId;
+  List<String> course;
+
+  Teacher(this.TeacherId, this.course):super('Mrs. Smith', 35, ' 456 Oak St');
+  
+  @override
+  void displayRole() {
+    print('Role: Teacher');
+  }
+
+  void displayCourse(){
+    for( var course in course){
+      print(course);
+    }
+  }
 }
 
 void main(){
-  StudentInfo studentInfo = StudentInfo();
-  print('Name : ${studentInfo.name}');
-  print('Address : ${studentInfo.address}');
- // print('Scors : ${studentInfo.scors}');
-  print('Average : ${studentInfo.average}');
+  print('This is for display Student information');
+  print('==============================================\n');
+  Student student = Student('', '', [82.0, 85.0, 90.0]);
+  student.displayRole();
+  print('Name: ${student.name}');
+  print('Age: ${student.age}');
+  print('Address: ${student.address}');
+  print('Average: ${student.clculateAvg()}\n');
+
+  //print('');
+  print('This is for display Teacher information');
+  print('==============================================\n');
+  Teacher teacher= Teacher('', ['Math', 'Bangla', 'English']);
+  teacher.displayRole();
+  print('Name: ${teacher.name}');
+  print('Age: ${teacher.age}');
+  print('Address: ${teacher.address}');
+  print('Courses Taught:');
+  teacher.displayCourse();
 }
